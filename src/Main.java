5bca3c5be7ae3de117e4b52056224b1d14a53c8e
Igor <igormedeiros407@gmail.com>
2024-07-22 00:11:41 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    /*
+    Algoritimo que ler um número digitado pelo usuario e imprima a tabuada
+    desse número.
+    */
+        int NumDigitado = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um numero e será mostrada a tabuada dele ", "Tabuada", JOptionPane.QUESTION_MESSAGE));
+    for (int i = 1; i < 10; i++){
+        System.out.printf("%d x %d = %d \n", NumDigitado, i, NumDigitado * i);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
